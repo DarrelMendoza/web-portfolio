ddbb2eb673a3bb871f3d35d3d19599e5b1f75763
Darrel Mendoza <darrelmendoza85@gmail.com>
2023-07-03 01:43:53 +0800

    feat(navbar-links): added scroll animation to navbars

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  isLinkActive(link: string): boolean {
+    return this.router.url === link;
+  }
+
+  toLandingPage(){
+    document.getElementById("landing-page").scrollIntoView({behavior:"smooth"});
+  }
+
+  toAboutPage(){
+    document.getElementById("about-page").scrollIntoView({behavior:"smooth"});
+  }
+
+  toExperiencePage(){
+    document.getElementById("github-graph").scrollIntoView({behavior:"smooth"});
+  }
+
+  toProjectsPage(){
+    document.getElementById("projects-page").scrollIntoView({behavior:"smooth"});
+  }
+
+  toContactsPage(){
+    document.getElementById("footer").scrollIntoView({behavior:"smooth"});
+  }
 }
