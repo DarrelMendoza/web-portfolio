b983aaf609e76d98d044b1f0c845ba12940d84dc
Darrel Mendoza <darrelmendoza85@gmail.com>
2023-07-05 01:35:17 +0800

    feat(project-modal): created carousel for displaying of images

--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 })
 export class ProjectsPageComponent implements OnInit {
   modalOpen: boolean = false;
+  // We set the first image as the currentImage by default
+  currentImage: string = './assets/sample.jpg';
+  // We create an array of the image sources
+  images: string[] = ['./assets/sample.jpg', './assets/sample2.jpg', './assets/sample3.jpg', './assets/sample3.jpg','./assets/sample3.jpg'];
 
   constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit(): void {  
     // Add global event listener on initialization
     this.renderer.listen('window', 'click',(event:Event)=>{this.onClickOutsideModal(event)});
+  }
+
+  // The changeImage function updates the currentImage based on the clicked image
+  changeImage(index: number): void {
+    this.currentImage = this.images[index];
   }
 
   openModal(): void {
