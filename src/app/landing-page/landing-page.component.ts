340be4739428c7105a3a8a2c0841e97ef7836245
Darrel Mendoza <darrelmendoza85@gmail.com>
2023-10-10 01:04:59 +0800

    fix(contributions-page): fixed contributions page and adjusted styles

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPageComponent implements OnInit {
 
   handleClick() {
-    window.open('https://drive.google.com/file/d/1E6MCgOgYuvTSRnpPKUJ5DS8TelxiMD8a/view', '_blank');
-    this.downloadFile('https://drive.google.com/uc?export=download&id=1E6MCgOgYuvTSRnpPKUJ5DS8TelxiMD8a');
+    window.open('https://drive.google.com/file/d/1l9BF9aa4M9e7pUbsSB21TywCcSnoePo8/view', '_blank');
+    this.downloadFile('https://drive.google.com/uc?export=download&id=1l9BF9aa4M9e7pUbsSB21TywCcSnoePo8');
   }
 
   downloadFile(url: string) {
